41ecfca9ccda19baf3bbbe111c4b86710d022d54
Python The Hack <lrulvoni@gmail.com>
2022-09-06 15:25:39 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+//import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StatusComponent } from './status/status.component';
+import { AddcontactComponent } from './addcontact/addcontact.component';
+//import { AccessRoutingModule } from './access/access-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    //LoginComponent,
+    ContactComponent,
+    StatusComponent,
+    AddcontactComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    //AccessRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
